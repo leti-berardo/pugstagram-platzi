2653532bdbc9bd8eb9bbde0efdf233a708e1d954
Leticia Berardo <leticiaberardo@gmail.com>
2020-06-10 15:27:12 -0300

    pugstagram plazti

---
+++ src/components/Footer.svelte
@@ -0,0 +1,35 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<style>
+  .Footer .Footer-content p span.copyleft {
+    display: inline-block;
+    text-align: right;
+    margin: 0px;
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    filter: FlipH;
+    -ms-filter: “FlipH”;
+  }
+
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-content">
+    <div class="Footer-copy">
+      <p>
+        <span class="copyleft">©</span>
+        {year} Berlinstagram from Platzi
+      </p>
+    </div>
+  </div>
+</div>
---
+++ src/containers/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { onMount } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Timeline from "../components/Timeline.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+
+  let data = {};
+  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
+<style>
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<Header />
+<Main>
+  <Timeline posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
